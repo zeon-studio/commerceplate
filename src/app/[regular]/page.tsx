95bf9b61cc4fff7bf41b29979913950c8a8e64c6
Al Murad Uzzaman <murad.themefisher@gmail.com>
2024-08-08 16:38:32 +0600

    not-found route handled

--- src/app/[regular]/page.tsx
+++ src/app/[regular]/page.tsx
@@ -1,30 +1,27 @@
 import MDXContent from "@/helpers/MDXContent";
 import { getListPage, getSinglePage } from "@/lib/contentParser";
-import CallToAction from "@/partials/CallToAction";
 import PageHeader from "@/partials/PageHeader";
 import SeoMeta from "@/partials/SeoMeta";
 import { RegularPage } from "@/types";
+import { notFound } from "next/navigation";
 
-// remove dynamicParams
-export const dynamicParams = false;
-
-// generate static params
+// Generate static params
 export const generateStaticParams = () => {
-  const getRegularPages = getSinglePage("pages");
-
-  const regularPages = getRegularPages.map((page: RegularPage) => ({
+  const regularPages = getSinglePage("pages").map((page: RegularPage) => ({
     regular: page.slug,
   }));
-
   return regularPages;
 };
 
-// for all regular pages
+// For all regular pages
 const RegularPages = ({ params }: { params: { regular: string } }) => {
   const regularData = getSinglePage("pages");
-  const data = regularData.filter(
-    (page: RegularPage) => page.slug === params.regular,
-  )[0];
+  const data = regularData.find(
+    (page: RegularPage) => page.slug === params.regular
+  );
+
+  if (!data) return notFound();
+
   const { frontmatter, content } = data;
   const { title, meta_title, description, image } = frontmatter;
   const callToAction = getListPage("sections/call-to-action.md");
